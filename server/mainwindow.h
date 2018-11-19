aeb823242eeedcd96904a1c9877ea8c3fb7075ba
LiuZichen <danteliuzichen@163.com>
2018-11-19 22:53:16 +0800

    可编译，不知到到什么程度

--- server/mainwindow.h
+++ server/mainwindow.h
@@ -2,7 +2,6 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QUdpSocket>
 #include <QTimer>
 #include <QDir>
 #include <QFileInfoList>
@@ -23,15 +22,13 @@ public:
 
 private slots:
     void NewConnect();
-    void SendMessage();
-    void GetMessage();
-    void SendDir();
+//    void SendMessage();
+    void GetMessage(QTcpSocket*);
+//    void SendDir();
     
 private:
     Ui::MainWindow *ui;
-    QUdpSocket m_UdpSocket,g_UdpSocket;
     QTcpServer *tcpServer;
-    QTcpSocket *tcpSocket;
 };
 
 #endif // MAINWINDOW_H
