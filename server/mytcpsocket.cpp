2aed62506119d00150b340c277d06975ea21587c
unknown <danteliuzichen@163.com>
2018-11-20 15:23:41 +0800

    该去上课了

--- server/mytcpsocket.cpp
+++ server/mytcpsocket.cpp
@@ -7,7 +7,13 @@ mytcpsocket::mytcpsocket(QTcpSocket* tcp)
 {
     m_tcp = tcp;
     qDebug()<<"new tcp"<<endl;
+    QByteArray data;
+    QDataStream out(&data,QIODevice::ReadWrite);
+    QString on_connect("220 hello");
+    out<<on_connect;
+    m_tcp->write(data);
     connect(m_tcp,SIGNAL(readyRead()),this,SLOT(getMessage()));
+    emit sendString(on_connect);
 }
 
 void mytcpsocket::getMessage()
