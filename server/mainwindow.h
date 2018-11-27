a21546570ec40f48061139697d4ff5755a7b3f67
LiuZichen <danteliuzichen@163.com>
2018-11-27 20:00:15 +0800

    修改注释

--- server/mainwindow.h
+++ server/mainwindow.h
@@ -19,15 +19,14 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    //获取信息添加服务器日志
     void GetMessage(QString);
+    //获取接收邮件内容
     void GetContent(QString);
 
 private slots:
+    //创建新的连接
     void NewConnect();
-    void test_message();
-//    void SendMessage();
-
-//    void SendDir();
     
 private:
     Ui::MainWindow *ui;
--- server/mytcpsocket.cpp
+++ server/mytcpsocket.cpp
@@ -27,28 +27,36 @@ mytcpsocket::mytcpsocket(QTcpSocket* tcp,MainWindow *p)
     this->pp->GetMessage(on_connect);
 }
 
+//获取客户端发送信息
 void mytcpsocket::getMessage()
 {
+    //获取客户端发送的信息
     QByteArray data = m_tcp->readAll();
-    qDebug()<<"debug:get message";
+    //对信息进行处理
     this->dealMessage(data);
 }
 
+//判断客户端发送内容种类，并作相应处理
 void mytcpsocket::dealMessage(QByteArray data)
 {
+    //将客户发送内容转为字符串
     QString str(data);
+    //处理该字符串，获取状态信息
     deal(str);
+    //当状态码为0，代表客户端请求断开链接，则将链接断开，并开始处理报文
     if (status == 0) {
         this->savefile(m_data);
         this->m_tcp->disconnect();
         this->dealContent();
     }
+    //当状态码为6，代表当前发送内容为报文，将所有报文叠加，等待最后处理
     if (status == 6)
     {
         m_data.append(data);
     }
 }
 
+//用于处理客户端发送的命令行
 void mytcpsocket::deal(QString str)
 {
     QStringList str_list = str.split(" ");
@@ -69,11 +77,11 @@ void mytcpsocket::deal(QString str)
         status = 4;
         res = "250 OK \r\n";
     }
-    else if (str.left(4) == "DATA" && status >= 4) {
+    else if (str_list[0] == "DATA" && status >= 4) {
         status = 5;
         res = "354 Enter mail, end with \".\" on a line by itself\r\n";
     }
-    else if( str.left(4) == "QUIT")
+    else if( str_list[0] == "QUIT")
     {
         status = 0;
         res = "221 Bye";
@@ -85,7 +93,6 @@ void mytcpsocket::deal(QString str)
         if(str.right(3) == ".\r\n")
         {
             res = "250 message sent \r\n";
-//              res = "502 message sent \r\n";
         }
 
     }
@@ -184,37 +191,38 @@ void mytcpsocket::dealContent()
     this->pp->GetContent(res);
 }
 
+//将base64编码格式的ASCII码转码为二进制格式的信息
 QByteArray mytcpsocket::frombase64(const QByteArray &data)
 {
     unsigned int buf = 0;
-        int nbits = 0;
-        QByteArray res((data.size() * 3) / 4, Qt::Uninitialized);
-        int offset = 0;
-        for (int i = 0; i < data.size(); ++i) {
-            int ch = data.at(i);
-            int d;
-            if (ch >= 'A' && ch <= 'Z')
-                d = ch - 'A';
-            else if (ch >= 'a' && ch <= 'z')
-                d = ch - 'a' + 26;
-            else if (ch >= '0' && ch <= '9')
-                d = ch - '0' + 52;
-            else if (ch == '+' )
-                d = 62;
-            else if (ch == '/' )
-                d = 63;
-            else
-                d = -1;
-            if (d != -1) {
-                buf = (buf << 6) | d;
-                nbits += 6;
-                if (nbits >= 8) {
-                    nbits -= 8;
-                    res[offset++] = buf >> nbits;
-                    buf &= (1 << nbits) - 1;
-                }
+    int nbits = 0;
+    QByteArray res((data.size() * 3) / 4, Qt::Uninitialized);
+    int offset = 0;
+    for (int i = 0; i < data.size(); ++i) {
+        int ch = data.at(i);
+        int d;
+        if (ch >= 'A' && ch <= 'Z')
+            d = ch - 'A';
+        else if (ch >= 'a' && ch <= 'z')
+            d = ch - 'a' + 26;
+        else if (ch >= '0' && ch <= '9')
+            d = ch - '0' + 52;
+        else if (ch == '+' )
+            d = 62;
+        else if (ch == '/' )
+            d = 63;
+        else
+            d = -1;
+        if (d != -1) {
+            buf = (buf << 6) | d;
+            nbits += 6;
+            if (nbits >= 8) {
+                nbits -= 8;
+                res[offset++] = buf >> nbits;
+                buf &= (1 << nbits) - 1;
             }
         }
-        res.truncate(offset);
-        return res;
+    }
+    res.truncate(offset);
+    return res;
 }
